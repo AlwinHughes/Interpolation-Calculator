06f4a2e9d689aad1b02f8c76c512960bb62404e3
Alwin Hughes <alwin.hughes@btopenworld.com>
2016-05-09 16:27:19 +0100

    bug fixes needed to fix calculating terms. other bits work

--- calculate.js
+++ calculate.js
@@ -6,29 +6,62 @@ var no_of_points = 4;
 function calculate () {
 	points = [];
 	block_size = 0;
-	coifitionts = [0,0,0,0];
+	coifitionts = [0,0,0,0,0];
 	for(var i = 0; i < 4;i++ ){
 		points.push([document.getElementById('X_'+i).value, document.getElementById('Y_'+i).value]);
 	}
 	
-	// combinations("11223344556677889900").forEach(function(item,index){
-	// 	console.log(item);
-	// 	console.log("index: "+index);
-	// })
-	console.log(combinations(getstring(points)));
-	
-	
-	// console.log(addzeroes("-1",true));
 
+	//console.log(selectAllBut(points,1));
+	
+ 	console.log(getstring(selectAllBut(points,1)));
+	var combs = combinations(getstring(selectAllBut(points,1)));
+	console.log(combs);
+
+	// for (var i = 0; i < points.length; i++) {// loop selects which x value is not in the top of the fraction (which term of the equation it is)
+	// 	var combs = combinations(getstring(selectAllBut(points,i)));
+	// 	console.log(combs);
+	// 	//calculating bottom of fraciton 
+	// 	var denominator = 1;
+		
+	// 	for(var j = 0; j< points.length; j++){
+	// 		if(i != j){
+	// 			// console.log("j"+j);
+
+	// 			denominator *= (points[i][0]-points[j][0]); 
+	// 		}
+	// 	}
+	// 	console.log("denominator:"+denominator);
+	// 	for (var k = 0; k < combs.length; k++) {
+	// 		console.log("k:"+k);
+	// 		console.log("comb:"+combs[k]);
+	// 		console.log("comb e:"+math.eval(combs[k]));
+	// 		console.log("length"+combs[k].replace(/[^*]/g, "").length);
+	// 		console.log("to add"+ math.eval(combs[k])*(points[i][1]/denominator));
+
+	// 		coifitionts[combs[k].replace(/[^*]/g, "").length+1] += math.eval(combs[k])*(points[i][1]/denominator);
+	// 	};
+		
+	// }
+
+	console.log(coifitionts);
 }
 
+function selectAllBut(array,not) {
+	var variable = [];
+	for (var i = 0; i < array.length; i++) {
+		if(i != not){
+			variable.push(array[i][0]);
+		}
+	};
+	return variable;
+}
 
 function logPoints(){
 	var points_string = "";
 	points.forEach(function(item,index){
 		points_string +='['+item[0]+','+item[1]+'],'
 	})
-
 	console.log(points_string);
 }
 
@@ -38,14 +71,14 @@ function getstring(data){
 	for (var i = 0; i < data.length; i++) {
 		console.log(i);
 		if(String(data[i][0]).length>block_size){
-			block_size = String(points[i][0]).length;
+			block_size = String(-points[i][0]).length;
 		}
 	};
 	console.log("block_size: "+block_size);
 	var return_string = "";
 
 	for (var i = 0; i < points.length; i++) {
-		return_string = return_string+ addzeroes(String(points[i][0]));
+		return_string = return_string+ addzeroes(String(-points[i][0]),(-points[i][0])<0);
 	}
 	console.log(return_string);
 	return return_string;
