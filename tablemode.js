0a78fc01e6aafd8f4b44e5f6586e2228f9e85043
Alwin Hughes <alwin.hughes@btopenworld.com>
2016-05-15 21:56:59 +0100

    table mode now works

--- tablemode.js
+++ tablemode.js
@@ -1,14 +1,47 @@
-var table_coifits;
+var table_coifits;// table_coifits[i] is coifitiont in Ax^n where n = i
 var order = 3;
 var x_vals;
+var y_vals;
+
 function tablemodeCalculate(){
 	//set up
 	x_vals = [];
-	for (var i = 0; i < order; i++) {
+	table_coifits = [];
+	
+	//getting values
+	for (var i = 0; i < 3; i++) {
 		x_vals.push(document.getElementById("table_x_"+i).value);
 	};
-	console.log(x_vals);
+	
+	for (var i = 0; i <= order; i++) {
+		table_coifits.push(document.getElementById("coif_"+i).value);	
+	}
+	
+	y_vals = Array(x_vals.length);
+
+	//calculatign values
+	for (var i = 0; i < y_vals.length; i++) {
+		y_vals[i] = getYValue(x_vals[i],table_coifits);
+	};
+	
+	//displaying values
+	for (var i = 0; i < y_vals.length; i++) {
+		document.getElementById("table_y_"+i).innerHTML = y_vals[i];
+	};
+
 };
+
+
+function getYValue(x,coifitionts){
+	var return_val = 0;
+	for(var i = 0; i<coifitionts.length; i++){
+		console.log(Math.pow(x,i)*coifitionts[i]);
+		return_val += Math.pow(x,i)*coifitionts[i];
+	}
+	return return_val;
+}
+
+
 $(document).ready(function(){
 	$("#tablemode_higher_order").click(function(){
 		order++;
