da9a52b8d724f2bf305a6da484732684098a4f4a
Alwin Hughes <alwin.hughes@btopenworld.com>
2016-05-24 20:21:55 +0100

    added graphing

---
+++ plot.js
@@ -0,0 +1,33 @@
+var function_string;
+function plotGraph () {
+	function_string = "";
+	math.config({
+  		number: 'number'
+	});
+	try{
+		if(coifitionts){
+			for (var i = 0; i <coifitionts.length; i++) {
+				
+				if(coifitionts[i]>0){
+					function_string += "+" + coifitionts[i]+"*x^"+(coifitionts.length-i-1); 
+				}else{
+					function_string += "-" + coifitionts[i]+"*x^"+(coifitionts.length-i-1);
+				}
+			};
+		}
+		if(function_string.charAt(0)=="+"){
+			latex_string = latex_string.substring(1);
+		}
+		console.log(function_string);
+		functionPlot({
+			target: "#plot",
+			data: [{
+				fn: function_string,
+				sampler: 'builtIn',
+				graphType: 'polyline'
+			}]
+		});
+	}catch(e){
+		alert("could not plot: "+e );
+	}
+}
